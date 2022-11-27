9955faf9be694642d3c7d9dad65088c261d43f8b
Lonskoy <lonskoy0304@gmail.com>
2022-11-27 23:06:28 +0300

    edit

--- task.js
+++ task.js
@@ -1,121 +1,36 @@
-const win = 0; // Победа = 10 попаданий
-const miss = 0;// Проигрыш = 5 промахов
-const hole1 = document.getElementById('hole1');
-const hole2 = document.getElementById('hole2');
-const hole3 = document.getElementById('hole3');
-const hole4 = document.getElementById('hole4');
-const hole5 = document.getElementById('hole5');
-const hole6 = document.getElementById('hole6');
-const hole7 = document.getElementById('hole7');
-const hole8 = document.getElementById('hole8');
-const hole9 = document.getElementById('hole9');
+let accWin = 0;                              //аккумулятор кликов по коту
+let accMiss = 0;                             //аккумулятор промахов
+let win = document.getElementById('dead');
+let miss = document.getElementById('lost');
 
-hole1.onclick = function(win, miss) {
-	if(hole1.className = 'hole_has-mole') {
-		win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
+function checkHole() {                        //Делает 9 иттераций проверок
+	for (let i = 1; i < 10; i++) {
+		let hole = document.getElementById('hole' + i);
+		hole.click = function() {
+			if(hole.className !== 'hole') {
+				accWin += 1;
+			}
+			else {
+				accMiss += 1;
+			}
+		}
 	}
 }
 
-hole2.onclick = function(win, miss) {
-	if(hole2.className = 'hole_has-mole') {
-			win += 1;
-		return win;
+const timer = setInterval(() => {
+	win.textContent = accWin;
+	miss.textContent = accMiss;
+	checkHole();
+	if(win === 10) {
+		clearInterval(timer);
+		alert('Вы победили!');
+		accWin = 0;
+		accMiss = 0;
 	}
-	else {
-		miss += 1;
-		return miss;
+	if(miss === 5) {
+		clearInterval(timer);
+		alert('Вы проиграли!');
+		accWin = 0;
+		accMiss = 0;
 	}
-}
-
-
-hole3.onclick = function(win, miss) {
-	if(hole3.className = 'hole_has-mole') {
-			win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
-	}
-}
-
-
-hole4.onclick = function(win, miss) {
-	if(hole4.className = 'hole_has-mole') {
-			win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
-	}
-}
-
-
-hole5.onclick = function(win, miss) {
-	if(hole5.className = 'hole_has-mole') {
-			win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
-	}
-}
-
-
-hole6.onclick = function(win, miss) {
-	if(hole6.className = 'hole_has-mole') {
-			win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
-	}
-}
-
-
-hole7.onclick = function(win, miss) {
-	if(hole7.className = 'hole_has-mole') {
-			win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
-	}
-}
-
-
-hole8.onclick = function(win, miss) {
-	if(hole8.className = 'hole_has-mole') {
-			win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
-	}
-}
-
-
-hole9.onclick = function(win, miss) {
-	if(hole9.className = 'hole_has-mole') {
-			win += 1;
-		return win;
-	}
-	else {
-		miss += 1;
-		return miss;
-	}
-}
-
-
-
-
+  }, 100);
