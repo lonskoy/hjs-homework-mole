58dabad67cdb9e31a9879f6c7a829748e4a21b9d
Lonskoy <lonskoy0304@gmail.com>
2022-11-27 19:15:45 +0300

    add files

---
+++ task.js
@@ -0,0 +1,121 @@
+const win = 0; // Победа = 10 попаданий
+const miss = 0;// Проигрыш = 5 промахов
+const hole1 = document.getElementById('hole1');
+const hole2 = document.getElementById('hole2');
+const hole3 = document.getElementById('hole3');
+const hole4 = document.getElementById('hole4');
+const hole5 = document.getElementById('hole5');
+const hole6 = document.getElementById('hole6');
+const hole7 = document.getElementById('hole7');
+const hole8 = document.getElementById('hole8');
+const hole9 = document.getElementById('hole9');
+
+hole1.onclick = function(win, miss) {
+	if(hole1.className = 'hole_has-mole') {
+		win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+hole2.onclick = function(win, miss) {
+	if(hole2.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+hole3.onclick = function(win, miss) {
+	if(hole3.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+hole4.onclick = function(win, miss) {
+	if(hole4.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+hole5.onclick = function(win, miss) {
+	if(hole5.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+hole6.onclick = function(win, miss) {
+	if(hole6.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+hole7.onclick = function(win, miss) {
+	if(hole7.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+hole8.onclick = function(win, miss) {
+	if(hole8.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+hole9.onclick = function(win, miss) {
+	if(hole9.className = 'hole_has-mole') {
+			win += 1;
+		return win;
+	}
+	else {
+		miss += 1;
+		return miss;
+	}
+}
+
+
+
+
